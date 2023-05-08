aa4606d063713af9f9ddd431cb5eb329970d2fc8
KarthikeyaPatnala12 <82384171+KarthikeyaPatnala12@users.noreply.github.com>
2023-05-08 22:31:22 +0530

    Update main.py
    Some minor changes

--- main.py
+++ main.py
@@ -5,23 +5,22 @@ import os
 import urllib.parse
 import warnings
 
-# specify the URL of the website you want to scrape
 url = input("Enter the URL of the website you want to scrape: ")
 file_ext = input("Enter the file extension you want to scrape (e.g., .pdf, .pptx, .py): ")
 
 warnings.filterwarnings("ignore", message="Unverified HTTPS request")
-# check if the URL has a scheme, and add one if necessary
+
 if not urllib.parse.urlparse(url).scheme:
     url = "https://" + url
 
 response = requests.get(url, verify=False)
 
-# parse the HTML response using BeautifulSoup
+
 soup = BeautifulSoup(response.content, "html.parser")
 
 website_name = url.split("//")[1].split(".")[0]
 
-# find all links on the page that point to File files
+
 file_links = []
 for link in soup.find_all("a"):
     href = link.get("href")
@@ -36,14 +35,14 @@ with open(file_links_file_name, "w", newline="") as f:
     for link in file_links:
         writer.writerow([link])
 
-# print out the File links
+
 print(f"{file_ext.upper()} links found on the website:")
 for link in file_links:
     print(link)
 
 print(f"{file_ext.upper()} links saved to {os.path.abspath(file_links_file_name)}")
 
-# ask the user if they want to scrape all links
+
 scrape_all_links = input("Do you want to scrape all links on the website? (y/n): ").lower()
 
 if scrape_all_links == 'y':
@@ -62,7 +61,7 @@ if scrape_all_links == 'y':
         for link in links:
             writer.writerow([link])
 
-    # print out all links
+    # prints out all links
     print("All links found on the website:")
     for link in links:
         print(link)
